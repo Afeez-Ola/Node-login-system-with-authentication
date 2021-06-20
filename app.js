243a9ea0e8ae2ad0f8610d331001798c9e9e6f68
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-20 17:41:28 +0100

    Change body-parser to express in middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
