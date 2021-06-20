a4d1e66338ff82e0161ae47c9bd649352d6051c6
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-20 17:21:57 +0100

    🏃 ON:

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+// jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const indexRouter = require('./routes/index');
+const userPage = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/user', userPage);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`We are live on PORT: ${PORT}`)
+    console.log(`We are live on PORT: ${PORT}`);
 });
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+// jshint esversion:6
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Registration Page');
+});
+
+router.get('/login', (req, res) => {
+    res.send('Login Page');
+});
+
+
+module.exports = router;
