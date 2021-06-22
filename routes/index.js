f7989e48e9278713bb1f89282a9104a48e019640
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-22 14:52:46 +0100

    Added authentication to the app

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 // jshint esversion:6
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/', (req, res) => {
     res.render('welcome');
 });
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', { userName: req.user.name });
 });
 
 
