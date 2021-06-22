17cdd3568e6a5afbcb251f0e818382b9389c9721
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-22 15:41:02 +0100

    👍 DONE:

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
 const indexRouter = require('./routes/index');
 const userPage = require('./routes/users');
